08dd669e13e2015a2e738efc929df49f5ed3a422
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-21 14:45:20 -0600

    put routing in it's own module

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,16 @@
+import { PageNotFoundComponent } from './page-not-found.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent, pathMatch: 'full' },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
