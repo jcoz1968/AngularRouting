48ee9f8b9d35e120cc698f029020e9474251e453
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-23 00:26:57 -0600

    Lazy Loading

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,11 +2,18 @@ import { PageNotFoundComponent } from './page-not-found.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthGuard } from './user/auth.guard';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent, pathMatch: 'full' },
+      {
+        path: 'products',
+        canActivate: [AuthGuard],
+        // canDeactivate: [ProductEditGuard],
+        loadChildren: './products/product.module#ProductModule'
+      },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', component: PageNotFoundComponent },
     ])
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -11,37 +11,28 @@ import { ProductResolver } from './product-resolver.service';
 
 import { SharedModule } from '../shared/shared.module';
 
-import { AuthGuard } from './../user/auth.guard';
-
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       {
-        path: 'products',
-        canActivate: [AuthGuard],
-        canDeactivate: [ProductEditGuard],
+        path: '',
+        component: ProductListComponent
+      },
+      {
+        path: ':id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver }
+      },
+      {
+        path: ':id/edit',
+        component: ProductEditComponent,
+        resolve: { resolvedData: ProductResolver },
         children: [
-          {
-            path: '',
-            component: ProductListComponent
-          },
-          {
-            path: ':id',
-            component: ProductDetailComponent,
-            resolve: { resolvedData: ProductResolver }
-          },
-          {
-            path: ':id/edit',
-            component: ProductEditComponent,
-            resolve: { resolvedData: ProductResolver },
-            children: [
-              { path: '', redirectTo: 'info', pathMatch: 'full' },
-              { path: 'info', component: ProductEditInfoComponent },
-              { path: 'tags', component: ProductEditTagsComponent },
-              { path: '', redirectTo: 'info', pathMatch: 'full' },
-            ]
-          }
+          { path: '', redirectTo: 'info', pathMatch: 'full' },
+          { path: 'info', component: ProductEditInfoComponent },
+          { path: 'tags', component: ProductEditTagsComponent },
+          { path: '', redirectTo: 'info', pathMatch: 'full' }
         ]
       }
     ])
