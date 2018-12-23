5d5917adf1b3b1769d36c876a52807e6a1766c73
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-22 18:31:14 -0600

    reading resolver with observable

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -26,9 +26,11 @@ export class ProductEditComponent implements OnInit {
   }
 
   ngOnInit() {
-    const resolvedData: ProductResolved = this.route.snapshot.data['resolvedData'];
-    this.errorMessage = resolvedData.error;
-    this.onProductRetrieved(resolvedData.product);
+    this.route.data.subscribe(data => {
+      const resolvedData: ProductResolved = data['resolvedData'];
+      this.errorMessage = resolvedData.error;
+      this.onProductRetrieved(resolvedData.product);
+    });
   }
 
   onProductRetrieved(product: Product): void {
