e0b8c54977702c92216feea6ac98ce3c3584e125
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-22 18:12:35 -0600

    product resolver error handling

--- src/app/products/product-resolver.service.ts
+++ src/app/products/product-resolver.service.ts
@@ -1,20 +1,33 @@
+import { catchError, map } from 'rxjs/operators';
 import { Injectable, Inject } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { Observable } from 'rxjs';
-import { Product } from './product';
+import { Observable, of } from 'rxjs';
+import { Product, ProductResolved } from './product';
 import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolver implements Resolve<Product> {
+export class ProductResolver implements Resolve<ProductResolved> {
 
   constructor(private productService: ProductService) {}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ProductResolved> {
     const id = +route.paramMap.get('id');
-    return this.productService.getProduct(id);
+    if (isNaN(+id)) {
+      const msg = `Product id was not a number ${id}`;
+      // console.error(msg);
+      return of({ product: null, error: msg });
+    }
+    // return this.productService.getProduct(id);
+    return this.productService.getProduct(+id)
+      .pipe(
+        map(product => ({ product: product })),
+        catchError(error => {
+          const msg = `Retrieval error ${error}`;
+          return of({ product: null, error: msg });
+        })
+      );
   }
-
 }
